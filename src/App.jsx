dda9a11c39e5905066bb7d1bd66175c79ffc185c
MRUDHULA SHRI M <mrudhulashrimanikantan@gmail.com>
2024-02-04 13:54:35 +0530

    fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Nav from './components/nav';
+import Nav from './components/Nav';
 import Home from './components/Home';
 import About from './components/About';
 import Details from './components/Details';
