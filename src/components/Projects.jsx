cb5de3c79cd9bbdf33e98d391bf9882021faa975
MRUDHULA SHRI M <mrudhulashrimanikantan@gmail.com>
2024-02-10 09:48:09 +0530

    project changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,7 +16,7 @@ function Project() {
       <h1 className='text-2xl lg:text-3xl font-bold mb-4 text-black text-center mt-16'>Projects</h1>
         </div>
         <div className=" max-w-screen-md mx-auto"> 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 p-5 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-5 ">
           {/* Plant Website Container */}
           <div className="max-w-full-md mx-auto rounded-xl shadowed-md overflow-hidden md:max-w-2xl hover:animate-pulse bg-white">
             {/* Image part */}
@@ -75,35 +75,7 @@ function Project() {
           </div>
         </div>
 
-        {/* Airline and Quiz Containers */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2 p-5">
-          {/* Airline Container */}
-          <div className="max-w-full-md mx-auto rounded-xl shadowed-md overflow-hidden md:max-w-2xl hover:animate-pulse bg-white">
-            {/* Image part */}
-            <div className="text-center p-8 ">
-              <img
-                src={air}
-                alt=""
-                width={200}
-                height={200}
-                className="border-2 border-black"
-              />
-            </div>
-
-            {/* Content part */}
-            <div className="p-4">
-              <div className="uppercase tracking-wide text-sm font-semibold">
-                Airline Reservation System
-              </div>
-              <p className="mt-2">Developed a Java-based application<br /> with JavaFXML for the front-end <br /> and utilized MySQL for the backend.</p>
-              <button
-                onClick={() => redirectToGitHub("https://github.com/Mrudhula06/Airline-reservation-System.git")}
-                className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                Code
-              </button>
-            </div>
-          </div>
 
           {/* Quiz Container */}
           <div className="max-w-full-md mx-auto bg-gray-300 rounded-xl shadowed-md overflow-hidden md:max-w-2xl hover:animate-pulse bg-white">
